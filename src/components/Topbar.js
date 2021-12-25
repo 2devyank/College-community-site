16f561430251e48d45cf123cac93a11dac2dbf24
dhankarsimran <83290371+dhankarsimran@users.noreply.github.com>
2021-12-25 21:48:02 +0530

    Update Topbar.js

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -63,47 +63,7 @@ const Topbar = (props) => {
                 Search
               </button>
             </form>
-
-            <div className="dropdown"></div>
-
-            <div className="d-flex align-items-center">
-              <a
-                className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                href="/"
-                role="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                  className="rounded-circle"
-                  height="45"
-                  alt="Black and White Portrait of a Man"
-                  loading="lazy"
-                />
-              </a>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a className="dropdown-item" href="/">
-                    My profile
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <Profile/>
           </div>
         </div>
       </nav>
