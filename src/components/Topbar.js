7b389be1d8ed6f71819aec90d04a3b6c465200cb
dhankarsimran <83290371+dhankarsimran@users.noreply.github.com>
2021-12-25 21:53:28 +0530

    Update Topbar.js

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -10,7 +10,7 @@ const Topbar = (props) => {
               src="http://adgitmdelhi.ac.in/wp-content/uploads/2018/07/logo-adgitm-small2.jpg"
               width={65}
               height={54}
-              alt='LOGO'
+              alt="LOGO"
               style={{ borderRadius: "50%", border: "solid white 3px" }}
             />
           </a>
@@ -28,42 +28,24 @@ const Topbar = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-
-              <li className="nav-item">
                 <a className="nav-link" href="/about">
                   About
                 </a>
               </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="/general">
-                  General
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="/technology">
-                  Technology
-                </a>
-              </li>
             </ul>
-            <form className="d-flex" style={{marginRight:'100px'}}>
+            <form className="d-flex" style={{ marginRight: "100px" }}>
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
-                style={{width:'600px'}}
+                style={{ width: "600px" }}
               />
               <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
             </form>
-            <Profile/>
+            <Profile />
           </div>
         </div>
       </nav>
