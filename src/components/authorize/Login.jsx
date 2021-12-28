5b79ac59ba5749bc01e10c1d4f5a6b8502e7b13b
Shubham Goel <sshubhamgoel24@gmail.com>
2021-12-28 09:50:05 +0530

    Firebase auth added

--- src/components/authorize/Login.jsx
+++ src/components/authorize/Login.jsx
@@ -1,9 +1,50 @@
 import React, { useState } from 'react';
 import '../../styles/Auth.css'
+import { getAuth, signInWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import app from './../../firebase';
+import { useNavigate } from 'react-router-dom';
+const auth = getAuth();
+
 function Login({toggleAuth}) {
+    const navigate = useNavigate();
+    
+    function loginuser(e){
+        e.preventDefault();
+        let password = e.target.password.value;
+        if(e.target.password.value.length < 6){
+            alert("Password too short");
+            return;
+        }
+        let email = e.target.email.value;
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            if(!user.emailVerified){
+                sendEmailVerification(auth.currentUser)
+                .then(() => {
+                    alert("Account not verified. Verification Mail Sent Again.")
+                    return;
+                });
+            }
+            else{
+                navigate('/home');
+            }
+        })
+        .catch((error) => {
+            if(error.code === 'auth/user-not-found'){
+                alert("User Not Registered");
+                return true;
+            }
+            console.log(error);
+        });
+
+    }
+
     return (
         <div className="Login" id='Login'>
-            <form action="/home">
+            <form onSubmit={loginuser}>
             <div className="container-fluid auth-container">
                 <div className="row toggle-row">
                     <div className="col-6 Login-Button">
@@ -16,19 +57,19 @@ function Login({toggleAuth}) {
 
                 <div className='row align-items-center'>
                     <div className='col-1'>
-                        <i  class="fas fa-user"></i>
+                        <i  className="fas fa-user"></i>
                     </div>
                     <div className='col'>
-                        <input type="text" placeholder='Email'/>
+                        <input type="text" name='email' placeholder='Email' required/>
                     </div>
                 </div>
 
                 <div className='row align-items-center'>
                     <div className='col-1'>
-                        <i class="fas fa-key"></i>
+                        <i className="fas fa-key"></i>
                     </div>
                     <div className='col'>
-                        <input type="password" placeholder='Password'/>
+                        <input type="password" name='password' placeholder='Password' required/>
                     </div>
                 </div>
 
--- src/components/authorize/Register.jsx
+++ src/components/authorize/Register.jsx
@@ -1,8 +1,51 @@
 import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
+import app from './../../firebase';
+const auth = getAuth();
+
+
 function Register({toggleAuth}) {
+    
+    function createuser(e){
+        e.preventDefault();
+        let password = e.target.password.value;
+        if(e.target.password.value.length < 6){
+            alert("Password too short");
+            return;
+        }
+        if(e.target.password.value != e.target.confirmPassword.value){
+            alert("Password Mismatch");
+            return;
+        }
+
+        let email = e.target.email.value;
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            //after user created
+            updateProfile(auth.currentUser, {
+                displayName: e.target.name.value
+            }).catch((error) => {
+                console.log(error);
+            });
+
+            sendEmailVerification(auth.currentUser)
+            .then(() => {
+                alert("Verification Mail Sent")
+            });
+
+            const usernew = userCredential.user;
+            console.log(usernew);
+            toggleAuth(true);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    }
+
     return (
         <div className="Register">
-            <form action="/home">
+            <form onSubmit={createuser}>
             <div className="container-fluid auth-container">
                 <div className="row toggle-row">
                     <div className="col-6 Login-Button">
@@ -15,37 +58,37 @@ function Register({toggleAuth}) {
 
                 <div className='row align-items-center '>
                     <div className='col-1'>
-                        <i class="fas fa-user"></i>
+                        <i className="fas fa-user"></i>
                     </div>
                     <div className='col'>
-                        <input type="text" placeholder='Name'/>
+                        <input type="text" name='name' placeholder='Name' required/>
                     </div>
                 </div>
 
                 <div className='row align-items-center'>
                     <div className='col-1'>
-                        <i class="fas fa-envelope"></i>
+                        <i className="fas fa-envelope"></i>
                     </div>
                     <div className='col'>
-                        <input type="text" placeholder='Email'/>
+                        <input type="text" name='email' placeholder='Email' required/>
                     </div>
                 </div>
 
                 <div className='row align-items-center'>
                     <div className='col-1'>
-                        <i class="fas fa-key"></i>
+                        <i className="fas fa-key"></i>
                     </div>
                     <div className='col'>
-                        <input type="password" placeholder='Password'/>
+                        <input type="password" name="password" placeholder='Password' required/>
                     </div>
                 </div>
                 
                 <div className='row align-items-center'>
                     <div className='col-1'>
-                        <i class="fas fa-key"></i>
+                        <i className="fas fa-key"></i>
                     </div>
                     <div className='col'>
-                        <input type="password" placeholder='Confirm Password'/>
+                        <input type="password" name='confirmPassword' placeholder='Confirm Password' required/>
                     </div>
                 </div>
 
